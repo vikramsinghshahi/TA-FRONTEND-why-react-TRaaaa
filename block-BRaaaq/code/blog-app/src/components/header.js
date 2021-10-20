4bace4d0184233835f0383570abb5fb956cf9349
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-10-20 12:02:16 +0530

    update

---
+++ block-BRaaaq/code/blog-app/src/components/header.js
@@ -0,0 +1,16 @@
+function Header(){
+    return (
+        <header>
+          <div className="container flex">
+            <h3>Gizmo</h3>
+            <ul className="flex">
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </header>
+      );
+}
+
+export default Header
